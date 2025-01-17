a44d7ed60ba26c25f105729c0c8077d95b1c7400
Psynona <jinceline1@gmail.com>
2025-01-17 12:06:39 +0100

    update navbar

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -2,8 +2,8 @@
     Composant qui affiche la barre de navigation
 -->
 <template>
-  <nav class=".navbar">
-    <router-link to="/" class="police_flix logo">F</router-link>
+  <nav :class="{ navbar: true, scrolled: isScrolled }">
+    <router-link to="/" class="logo"> <img class="logo" src="../assets/logo.png" alt="logo"> </router-link>
     <div class="navlinks">
       <ul>
         <!-- Filtre les liens avec showInNavbar à true -->
@@ -30,8 +30,8 @@ export default {
   name: 'Navbar',
   data() {
     return {
+      isScrolled: false, // Suivi de l'état de défilement
       navigationLinks: navigationLinks.map(link => {
-        // Associe les icônes aux liens correspondants
         const iconMap = {
           Accueil: homeIcon,
           'Recherche de films': searchIcon,
@@ -43,9 +43,22 @@ export default {
   },
   computed: {
     filteredLinks() {
-      // Filtre uniquement les liens où showInNavbar est true
       return this.navigationLinks.filter(link => link.showInNavbar);
     }
+  },
+  methods: {
+    handleScroll() {
+      // Vérifie si l'utilisateur a scrollé
+      this.isScrolled = window.scrollY > 50; // Par exemple, 50px de défilement
+    }
+  },
+  mounted() {
+    // Ajoute un écouteur d'événement scroll à la fenêtre
+    window.addEventListener('scroll', this.handleScroll);
+  },
+  beforeDestroy() {
+    // Nettoie l'écouteur pour éviter les fuites de mémoire
+    window.removeEventListener('scroll', this.handleScroll);
   }
 };
 
@@ -74,11 +87,13 @@ nav {
 }
 
 .logo {
-  font-size: 50px;
-  color: #243971;
-  padding: 0;
   margin-inline: 20px;
   list-style: none;
+}
+
+img {
+  width: 50px;
+  height: auto;
 }
 
 .navlinks ul {
@@ -101,8 +116,12 @@ a {
   text-decoration: none;
 }
 
-a.router-link-exact-active img {
+a.router-link-exact-active img:not(.logo img){
   opacity: 0.5;
+}
+
+.navlinks li[data-v-41458b80] {
+  padding: 0 1em ;
 }
 
 .navlinks img {
@@ -113,8 +132,8 @@ a.router-link-exact-active img {
   width: 42px;
 }
 
-.navbar-scrolled {
+.navbar.scrolled {
     background-color: rgb(29, 29, 29);
-    height: 3.4em;
+    height: 2.8em;
 }
 </style>
